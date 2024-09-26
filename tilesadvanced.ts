7eb06e2b50f47be5ceee9bbcc6e85259d2415275
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2024-09-26 16:01:00 +0100

    Restore '26/09/2024, 10:43:24 rebuilt animate tiles func to be endable'

--- tilesadvanced.ts
+++ tilesadvanced.ts
@@ -13,8 +13,6 @@ enum PathDirection { Up, Down, Left, Right }
 
 namespace tilesAdvanced {
 
-    export let tilesAnimated = false
-
     function adjacentTilesInPlus(tile: tiles.Location, distance: number): tiles.Location[] {
         let col = tile.col;
         let row = tile.row;
@@ -242,13 +240,8 @@ namespace tilesAdvanced {
         let frame = 0
         let tilesToAnimate = tiles.getTilesByType(tile)
         let tilemap = game.currentScene().tileMap.data
-        tilesAnimated = true
         control.runInBackground(function () {
             while (tilemap == game.currentScene().tileMap.data) {
-                if (!tilesAnimated){
-                    scene.setTileMapLevel(tilemap)
-                    break
-                }
                 for (let tileOfType of tilesToAnimate) {
                     tiles.setTileAt(tileOfType, animation[frame])
                 }
@@ -259,17 +252,6 @@ namespace tilesAdvanced {
                 pause(frameLength)
             }
         })
-    }
-
-    /**
-     * stops animation running on any animated tiles
-     */
-    //% blockId=stopTileAnimation
-    //% block="stop tile animations"
-    //% group="Tile Animation"
-    //% weight=5
-    export function stopTileAnimation(){
-        tilesAnimated = false
     }
 
     /**
